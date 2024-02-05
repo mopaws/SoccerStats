fc7658c4550cf63fca0f1fd5b6aa5f07c8cdce96
0ddSh33p <92823911+0ddSh33p@users.noreply.github.com>
2024-02-05 11:46:21 -0500

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,19 @@
 function tryToLogin(){
   let box = document.getElementById("keepLogin");
-  alert(box.checked);
-
   //check if the user exists in database
 
    localStorage.setItem("save login", box.checked);
    localStorage.setItem("logged in", true);
+
+   if(box.checked){
+     localStorage.setItem("user", document.getElementById("username").value);
+     localStorage.setItem("pass", document.getElementById("password").value);
+   } else {
+     if(localStorage.getItem("user")){
+       localStorage.removeItem("user");
+     }
+     if(localStorage.getItem("pass")){
+       localStorage.removeItem("pass");
+     }
+   }  
 }
