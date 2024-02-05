5454e6325d835bd55d83c03ccba87ff7c3b96b56
0ddSh33p <92823911+0ddSh33p@users.noreply.github.com>
2024-02-05 11:25:37 -0500

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -1,4 +1,9 @@
 function tryToLogin(){
   let box = document.getElementById("keepLogin");
   alert(box.value);
+
+  //check if the user exists in database
+
+   localStorage.setItem("save login", box.value);
+   localStorage.setItem("logged in", true);
 }
