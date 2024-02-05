6061a05ffefe1026d22208d86424c6be25d3b8b3
0ddSh33p <92823911+0ddSh33p@users.noreply.github.com>
2024-02-05 10:27:16 -0500

    Update model.js

--- js/model.js
+++ js/model.js
@@ -1,16 +1,8 @@
-const initSqlJs = window.initSqlJs;
+//Set up local storage login stuff if it hasn't already been created for the current user.
+if(! localStorage.getItem("save login")){
+  localStorage.setItem("save login", false);
+}
+if(! localStorage.getItem("logged in")){
+  localStorage.setItem("logged in", false);
+}
 
-const SQL = await initSqlJs({
-  // Required to load the wasm binary asynchronously. Of course, you can host it wherever you want
-  // You can omit locateFile completely when running in node
-  locateFile: file => `https://sql.js.org/dist/${file}`
-});
-
-// Create a database
-const db = new SQL.Database();
-
-// Execute a single SQL string that contains multiple statements
-let sqlstr = "CREATE TABLE testData (a int, b char); \
-INSERT INTO testData VALUES (0, 'hello'); \
-INSERT INTO testData VALUES (1, 'world');";
-db.run(sqlstr);
