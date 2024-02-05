40fedf2b93a04da22ed84b1a31f52d12003af084
0ddSh33p <92823911+0ddSh33p@users.noreply.github.com>
2024-02-05 08:52:50 -0500

    Update model.js

--- js/model.js
+++ js/model.js
@@ -1,1 +1,12 @@
-//var db = openDatabase('dataBase/MainDataBase', '1.0', 'database for storing statistics for the DRHS womens soccer team', 2 * 1024 * 1024);
+var db = openDatabase('mydb', '1.0', 'my first database', 2 * 1024 * 1024);
+db.transaction(function (tx) {
+  tx.executeSql('CREATE TABLE IF NOT EXISTS foo (id unique, text)');
+  tx.executeSql('INSERT INTO foo (id, text) VALUES (1, "synergies")');
+});
+
+tx.executeSql('SELECT * FROM foo', [], function (tx, results) {
+  var len = results.rows.length, i;
+  for (i = 0; i < len; i++) {
+    alert(results.rows.item(i).text);
+  }
+});
