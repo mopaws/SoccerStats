9a70af4f097c08d9276c4e36ce5855b489179e72
0ddSh33p <92823911+0ddSh33p@users.noreply.github.com>
2024-02-06 08:50:28 -0500

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -1,19 +1,28 @@
 function tryToLogin(){
+  // get refrences to the user inputs
   let box = document.getElementById("keepLogin");
-  //check if the user exists in database
+  let pass = document.getElementById("password");
+  let user = document.getElementById("username");
 
-   localStorage.setItem("save login", box.checked);
-   localStorage.setItem("logged in", true);
+  //Make sure the user name and pasword have been typed in
+  if(pass.value.length > 1 && user.value.length > 1){
+    
+    //check if the user exists in database
 
-   if(box.checked){
-     localStorage.setItem("user", document.getElementById("username").value);
-     localStorage.setItem("pass", document.getElementById("password").value);
-   } else {
-     if(localStorage.getItem("user")){
-       localStorage.removeItem("user");
-     }
-     if(localStorage.getItem("pass")){
-       localStorage.removeItem("pass");
-     }
-   }  
+      // if all checks are met, save the login if they want to, ans sign them in
+     localStorage.setItem("save login", box.checked);
+     localStorage.setItem("logged in", true);
+  
+     if(box.checked){
+       localStorage.setItem("user", user.value);
+       localStorage.setItem("pass", pass.value);
+     } else {
+       if(localStorage.getItem("user")){
+         localStorage.removeItem("user");
+       }
+       if(localStorage.getItem("pass")){
+         localStorage.removeItem("pass");
+       }
+     }  
+   }
 }
