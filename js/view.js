ee46b80a589cd1eb952a0cc36fdf948e9198b136
0ddSh33p <92823911+0ddSh33p@users.noreply.github.com>
2024-02-06 08:56:33 -0500

    Update view.js

--- js/view.js
+++ js/view.js
@@ -1,1 +1,11 @@
-
+window.onload = (event) => {
+  let pass = document.getElementById("password");
+  let user = document.getElementById("username");
+  
+  if(localStorage.getItem("user")){
+    user.value = localStorage.getItem("user");
+  }
+  if(localStorage.getItem("pass")){
+    pass.value = localStorage.getItem("pass");
+  }
+};
