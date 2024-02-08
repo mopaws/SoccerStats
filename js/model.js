80b634829c4e86162527d2ca48627669a295cac6
0ddSh33p <92823911+0ddSh33p@users.noreply.github.com>
2024-02-08 10:53:29 -0500

    Update model.js

--- js/model.js
+++ js/model.js
@@ -1,15 +1,36 @@
 //Set up local storage login stuff if it hasn't already been created for the current user.
-if(! localStorage.getItem("save login")){
-  localStorage.setItem("save login", false);
-}
-if(! localStorage.getItem("logged in")){
-  localStorage.setItem("logged in", false);
-}
+localStorage.setItem("save login", false);
 
-fetch('https://127.0.0.1:5000/getData')
+
+
+function generateSignInRequest(name, password){
+
+  let box = document.getElementById("keepLogin");
+  let pass = document.getElementById("password");
+  let user = document.getElementById("username");
+
+
+  fetch('http://127.0.0.1:5000/getData')
   .then(response => response.json())
   .then(data => {
-    console.log('Data received:', data);
-    // Do something with the data
+    for(let i = 0; i < data.length; i++){
+
+      if(data[i][0] == name && data[i][1] == password){
+        localStorage.setItem("save login", box.checked);
+        loadPage(true);
+        if(box.checked){
+          localStorage.setItem("user", user.value);
+          localStorage.setItem("pass", pass.value);
+        } else {
+          if(localStorage.getItem("user")){
+            localStorage.removeItem("user");
+          }
+          if(localStorage.getItem("pass")){
+            localStorage.removeItem("pass");
+          }
+        } 
+      }
+    }
   })
   .catch(error => console.error('Error:', error));
+}
