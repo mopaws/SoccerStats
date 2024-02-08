965b54bf563419de433ae5423001e2f98e203391
0ddSh33p <92823911+0ddSh33p@users.noreply.github.com>
2024-02-08 10:57:04 -0500

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -1,28 +1,28 @@
 function tryToLogin(){
   // get refrences to the user inputs
-  let box = document.getElementById("keepLogin");
   let pass = document.getElementById("password");
   let user = document.getElementById("username");
 
   //Make sure the user name and pasword have been typed in
   if(pass.value.length > 1 && user.value.length > 1){
-    
-    //check if the user exists in database
+    generateSignInRequest(user.value,pass.value);
+  }
+}
 
-      // if all checks are met, save the login if they want to, ans sign them in
-     localStorage.setItem("save login", box.checked);
-     localStorage.setItem("logged in", true);
-  
-     if(box.checked){
-       localStorage.setItem("user", user.value);
-       localStorage.setItem("pass", pass.value);
-     } else {
-       if(localStorage.getItem("user")){
-         localStorage.removeItem("user");
-       }
-       if(localStorage.getItem("pass")){
-         localStorage.removeItem("pass");
-       }
-     }  
-   }
+window.onload = (event) => {
+  //access login data fields.
+  let pass = document.getElementById("password");
+  let user = document.getElementById("username");
+  let keep = document.getElementById("keepLogin");
+
+  //if the data exists in local storage, autofill the page
+  if(localStorage.getItem("user")){
+    user.value = localStorage.getItem("user");
+  }
+  if(localStorage.getItem("pass")){
+    pass.value = localStorage.getItem("pass");
+  }
+  if(localStorage.getItem("save login")){
+    keep.checked = localStorage.getItem("save login");
+  }
 }
