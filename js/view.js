101d9f2c911ca406c71cf1f09161287ad9a112a4
0ddSh33p <92823911+0ddSh33p@users.noreply.github.com>
2024-02-08 10:57:39 -0500

    Update view.js

--- js/view.js
+++ js/view.js
@@ -1,17 +1,6 @@
-window.onload = (event) => {
-  //access login data fields.
-  let pass = document.getElementById("password");
-  let user = document.getElementById("username");
-  let keep = document.getElementById("keepLogin");
-
-  //if the data exists in local storage, autofill the page
-  if(localStorage.getItem("user")){
-    user.value = localStorage.getItem("user");
+function loadPage(logtest){
+  let logBox = document.getElementById("loginBox");
+  if(logtest == true){
+    logBox.remove();
   }
-  if(localStorage.getItem("pass")){
-    pass.value = localStorage.getItem("pass");
-  }
-  if(localStorage.getItem("save login")){
-    keep.checked = localStorage.getItem("save login");
-  }
-};
+}
