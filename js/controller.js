75816e6e103928da9448104bc26d1a2f10fc23f1
0ddSh33p <fastsmith3@gmail.com>
2024-02-27 20:58:12 -0500

    YIPEE

--- js/controller.js
+++ js/controller.js
@@ -1,19 +1,3 @@
-//access login data fields.
-let pass = document.getElementById("password");
-let user = document.getElementById("username");
-let keep = document.getElementById("keepLogin");
-
-//if the data exists in local storage, autofill the page
-if(localStorage.getItem("user")){
-  user.value = localStorage.getItem("user");
-}
-if(localStorage.getItem("pass")){
-  pass.value = localStorage.getItem("pass");
-}
-if(localStorage.getItem("save login")){
-  keep.checked = localStorage.getItem("save login");
-}
-
 function tryToLogin(){
   // get refrences to the user inputs
   let pass = document.getElementById("password");
@@ -29,25 +13,26 @@ function tryCreateUser(){
   let n = document.getElementById("newName");
   let p = document.getElementById("newPass");
   let cp = document.getElementById("confPass");
-  let vis = "0";
 
-  if(document.getElementById("vTypes").value == 1){
-    vis =  document.getElementById("visi").value;
-  }
-
-  if(n.value.length > 0 && p.value.length > 0 && p.value == cp.value){
-    createNewUser(n.value, p.value);
+  if(n.value.length > 0 && p.value.length > 2 && p.value == cp.value){
+    createNewUser(n.value,p.value);
   }
 }
 
-function updateVcheck(){
-  let field = document.getElementById("viewSelector");
-  let type = document.getElementById("vTypes");
+window.onload = (event) => {
+  //access login data fields.
+  let pass = document.getElementById("password");
+  let user = document.getElementById("username");
+  let keep = document.getElementById("keepLogin");
 
-  if(type.value == 1){
-    field.innerHTML = '<p>please enter the player jersy below </p> <input type = "text", id = visi>';
+  //if the data exists in local storage, autofill the page
+  if(localStorage.getItem("user")){
+    user.value = localStorage.getItem("user");
+  }
+  if(localStorage.getItem("pass")){
+    pass.value = localStorage.getItem("pass");
+  }
+  if(localStorage.getItem("save")){
+    keep.checked = localStorage.getItem("save");
   }
 }
-
-  
-
--- js/model.js
+++ js/model.js
@@ -1,6 +1,6 @@
 //Set up local storage login stuff if it hasn't already been created for the current user.
-if(!localStorage.getItem("save login")){
-  localStorage.setItem("save login", false);
+if(! localStorage.getItem("save")){
+  localStorage.setItem("save", false);
 }
 
 function generateSignInRequest(name, password){
