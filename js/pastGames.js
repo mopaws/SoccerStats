2434db4d054137485db771788ced9156106247f9
0ddSh33p <fastsmith3@gmail.com>
2024-04-19 10:57:04 -0400

    Past games done

--- js/pastGames.js
+++ js/pastGames.js
@@ -8,7 +8,7 @@ function buildPage(){
     .then(response => response.json())
     .then(data => {
         for(let i = 0; i < data.length; i++){
-            side.innerHTML += "<a onclick = 'fillData("+data[i][0]+")'>"+ data[i][1] + "\nVS " + data[i][2] +"</a> "
+            side.innerHTML += "<a onclick = 'fillData("+data[i][0]+")', class = 'sides'>"+ data[i][1] + "\n " + data[i][2] +"</a> "
         }
     })
     .catch(error => console.error('Error:', error));
@@ -16,6 +16,10 @@ function buildPage(){
 }
 function fillData(id){
     gameData.innerHTML = "";
+    let row = gameData.insertRow();
+    let cell = row.insertCell();
+    cell.innerHTML = "<h2>Game</h2>";
+
     fetch("http://127.0.0.1:5000/fetchStats/" + id)
    .then(response => response.json())
    .then(data => {
@@ -31,14 +35,20 @@ function fillData(id){
             .then(data2 => {
                 for(let i = 0; i < data2.length; i++){
                     if(data2[i][1] == data[j][0]){
-                        row = gameData.insertRow();
+                        let row = gameData.insertRow();
+
+                        cell = row.insertCell();
+                        cell.innerHTML = "<b>"+data[j][1]+"</b>";
 
                         if(data2[i][4]){
                             cell = row.insertCell();
                             cell.innerHTML = "<p> player: " + data2[i][4] + "</p>";
                         }
 
-                        cell.innerHTML = "<p> number: " + data2[i][3] + "</p>";
+                        if(data2[i][3]){
+                            cell = row.insertCell();
+                            cell.innerHTML = "<p> number: " + data2[i][3] + "</p>";
+                        }
 
                         if(data2[i][5]){
                             cell = row.insertCell();
