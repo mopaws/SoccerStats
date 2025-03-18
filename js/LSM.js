e0d32616c44e86509778a1e9e5851dead80ad52e
iamdefinitelynotkatherine <128514291+iamdefinitelynotkatherine@users.noreply.github.com>
2025-03-17 21:06:13 -0400

    Update LSM.js

--- js/LSM.js
+++ js/LSM.js
@@ -1,1 +1,24 @@
 //local storage
+/*
+class LocalStorageManager {
+    constructor(model, lsKey) {
+        this.lsKey = lsKey;
+        let existingValues = JSON.parse(localStorage.getItem(lsKey));
+        if(existingValues) {
+            for(let val of existingValues){
+                model.addRoll(val);
+            }
+        }
+        
+        model.subscribe(function(scope, msg) {
+            if(msg === 'updatedRoll') {
+                this.save(scope.getRolls());
+            }
+        }.bind(this));
+    }
+    save(data) {
+        localStorage.setItem(this.lsKey, JSON.stringify(data));
+    }
+    
+}
+*/
