2364309f69d657449adcccf9c5cd49e31a82aed0
iamdefinitelynotkatherine <128514291+iamdefinitelynotkatherine@users.noreply.github.com>
2025-04-11 11:24:32 -0400

    Create login.js

---
+++ login.js
@@ -0,0 +1,12 @@
+const tryLogin‎ = document.getElementById('tryLogin‎');
+const username‎ = document.getElementById('username‎');
+const password‎ = document.getElementById('password‎');
+
+function trylogin(){
+  if(username.value == 'admin' && password.value == 'SoccerStats2025'){
+    window.location.href = "https://https://en.wikipedia.org/wiki/Aqueduct_(water_supply)?scrlybrkr=4ce004b0";
+  }
+  
+}
+
+tryLogin.addEventListener('click',trylogin);
