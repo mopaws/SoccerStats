9ca9c7dc4fe6145ec97e372d8cd778d33cc9beaa
mopaws <150943095+mopaws@users.noreply.github.com>
2025-04-11 11:30:18 -0400

    Create init_db.py

---
+++ init_db.py
@@ -0,0 +1,23 @@
+import os
+import psycopg2
+
+conn = psycopg2.connect(
+        host="drhscit.org",
+        database=os.environ['DB'],
+        user=os.environ['DB_UN'],
+        password=os.environ['DB_PW'])
+
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS scouting_report;')
+
+cur.execute('CREATE TABLE scouting_report (id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,'
+                                 'team TEXT NOT NULL,'
+                                 'players_to_watch TEXT,'
+                                 'team_notes TEXT,'
+                                 'date_added DATE DEFAULT CURRENT_DATE);'
+                                 )
+conn.commit()
+
+cur.close()
+conn.close()
