d7b5110a699d4a9af56025583d96e791eb655f8c
iamdefinitelynotkatherine <128514291+iamdefinitelynotkatherine@users.noreply.github.com>
2025-04-15 09:22:41 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,11 +1,11 @@
-const tryLogin‎ = document.getElementById('tryLogin‎');
-const username‎ = document.getElementById('username‎');
-const password‎ = document.getElementById('password‎');
+var tryLogin‎ = document.getElementById('tryLogin‎');
+const username‎ = document.getElementById('username‎').value;
+const password‎ = document.getElementById('password‎').value;
 const tester‎ = document.getElementById('tester‎');
 
 tryLogin.addEventListener('click', function() {
-  if(username.value == 'a' && password.value == '2025'){
-    tester.innerText = 'it works!!!'
+  if(username == 'a' && password == '2025'){
+    tester.innerText = 'it works!!!';
     window.location.href = "Past_Games_Page.html";
   }
   
