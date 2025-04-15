212453450cbb4a7c3e869dc70b2258fb949ba64f
iamdefinitelynotkatherine <128514291+iamdefinitelynotkatherine@users.noreply.github.com>
2025-04-15 08:53:33 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,9 +1,11 @@
 const tryLogin‎ = document.getElementById('tryLogin‎');
 const username‎ = document.getElementById('username‎');
 const password‎ = document.getElementById('password‎');
+const tester‎ = document.getElementById('tester‎');
 
 function goToHome(){
   if(username.value == 'admin' && password.value == 'SoccerStats2025'){
+    tester.innerText = 'it works!!!'
     window.location.href = "Past_Games_Page.html";
   }
   
