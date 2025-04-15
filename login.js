8241c56b6889df6dd7f8bacd58707db0200643c1
mopaws <150943095+mopaws@users.noreply.github.com>
2025-04-15 13:06:28 -0400

    Update login.js

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ var tryLogin‎ = document.getElementById('tryLogin‎');
 var tester‎ = document.getElementById('tester‎');
 
 tryLogin.addEventListener('click', function() {
-  var username‎ = document.getElementById('username‎').value;
+  var username‎ = document.getElementById('username‎').value; 
   var password‎ = document.getElementById('password‎').value;
   if(username == 'a' && password == '2025'){
     tester.innerText = 'it works!!!';
