9cabd3411989a61ea72bb2b2cad304dfdd60d220
mopaws <150943095+mopaws@users.noreply.github.com>
2025-04-15 14:14:38 -0400

    Update login.js

--- login.js
+++ login.js
@@ -1,19 +1,19 @@
 function goToHome(){
-const tryLogin‎ = document.getElementById('tryLogin‎'); 
-const tester‎ = document.getElementById('tester‎');
-//tryLogin.addEventListener('click', function() {
-  const username‎ = document.getElementById('username‎').value; 
-  const password‎ = document.getElementById('password‎').value;
-  if(username == 'a' && password == '2025'){
-    tester.innerText = 'it works!!!';
-    window.location.href = "Past_Games_Page.html";
-  }
-  else{
-    tester.innerText = 'Invalid username or password';
-  }
-}
-
-tryLogin.addEventListener('click',goToHome);
+    const tryLogin = document.getElementById('tryLogin');
+    const tester = document.getElementById('tester');
+    //tryLogin.addEventListener('click', function() {
+      const username = document.getElementById('username').value; 
+      const password = document.getElementById('password').value;
+      if(username == 'a' && password == '2025'){
+        tester.innerText = 'it works!!!';
+        window.location.href = "Past_Games_Page.html";
+      }
+      else{
+        tester.innerText = 'Invalid username or password';
+      }
+    }
+    
+    tryLogin.addEventListener('click',goToHome);
 /*
 var tryLogin‎ = document.getElementById('tryLogin‎');
 var tester‎ = document.getElementById('tester‎');
