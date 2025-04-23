1238e40672dc3f4fb4d854ad6a64d00fb5e64134
mopaws <150943095+mopaws@users.noreply.github.com>
2025-04-23 18:32:43 -0400

    Update login.js

--- login.js
+++ login.js
@@ -13,22 +13,3 @@ function goToHome(){
     }
     
     tryLogin.addEventListener('click',goToHome);
-/*
-    //tryLogin.addEventListener('click', function() {
-var tryLogin‎ = document.getElementById('tryLogin‎');
-var tester‎ = document.getElementById('tester‎');
-  var username‎ = document.getElementById('username‎').value; 
-  var password‎ = document.getElementById('password‎').value;
-tryLogin.addEventListener('click', function() {
-  if(username == 'a' && password == '2025'){
-    tester.innerText = 'it works!!!';
-    window.location.href = "Past_Games_Page.html";
-  }
-  else{
-    tester.innerText = 'Invalid username or password';
-  }
-}
-});
-
-tryLogin.addEventListener('click',goToHome);
-*/
